e3b23c739d9b69ad21182cdde1bf4a554633648d
Anton Ibragimov <anton.ibragimov@angloamerican.com>
2023-06-20 21:17:09 +0300

    add flux on kind gitops infra

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  description = "GCP project to use"
+}
+
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1"
+  description = "GCP region to use"
+}
+
+variable "GOOGLE_ZONE" {
+  type        = string
+  default     = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  description = "GitHub owner repository to use"
+}
+
+variable "GITHUB_TOKEN" {
+  type        = string
+  description = "GitHub personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type        = string
+  default     = "kbot-flux-gitops"
+  description = "Flux GitOps repository"
+}
