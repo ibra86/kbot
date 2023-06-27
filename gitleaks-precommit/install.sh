4486e0a97a0edcf49e5821527aa958f09d486070
Anton Ibragimov <ibrahimov.ag@gmail.com>
2023-06-27 16:12:05 +0300

    add mkdirs for gitleaks install

--- gitleaks-precommit/install.sh
+++ gitleaks-precommit/install.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-mkdir gitleaks-precommit
-cd gitleaks-precommit
+GITLEAKS_BASE_DIR=gitleaks-precommit
+
+mkdir $GITLEAKS_BASE_DIR
+cd $GITLEAKS_BASE_DIR
 git clone https://github.com/gitleaks/gitleaks.git
 cd gitleaks
 make build
+
+mkdir $GITLEAKS_BASE_DIR/bin
+PATH=$GITLEAKS_BASE_DIR/bin:$PATH
+
+# add githook pre-commit
+# add option: git config --bool hooks.gitleaks
 
 echo 'gitleaks is installed'
 
