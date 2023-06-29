f61e513183c06bbe397d1d8d784d3320ddd1cb4c
Anton Ibragimov <ibrahimov.ag@gmail.com>
2023-06-29 15:36:07 +0300

    add wi and kms resources

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -47,3 +47,24 @@ module "flux_bootstrap" {
 
   github_token = var.GITHUB_TOKEN
 }
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux-2"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+}
